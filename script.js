36af599dec4d35e6e04550f857bdd1c2c4d87d86
soroushpiri-glitch <soroush.piri@morgan.edu>
2026-02-27 18:56:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,27 @@ function sayWelcome() {
     const audio = new Audio("assets/Welcome.m4a");
     audio.play();
 }
+
+function sayWelcome() {
+    const audio = new Audio("assets/welcome.m4a");
+    audio.play();
+}
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach(counter => {
+  const updateCount = () => {
+    const target = +counter.getAttribute('data-target');
+    const count = +counter.innerText;
+
+    const increment = target / 100;
+
+    if (count < target) {
+      counter.innerText = Math.ceil(count + increment);
+      setTimeout(updateCount, 20);
+    } else {
+      counter.innerText = target.toLocaleString() + '+';
+    }
+  };
+
+  updateCount();
+});
